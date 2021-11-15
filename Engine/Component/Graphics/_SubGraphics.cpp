0552dc5281b3bfb3343c573e1db5a0c37789656a
kbmhansungb <56149613+kbmhansungb@users.noreply.github.com>
2021-11-15 20:10:09 +0900

    SubGraphics 리펙토링

--- Engine/Component/Graphics/_SubGraphics.cpp
+++ Engine/Component/Graphics/_SubGraphics.cpp
@@ -25,34 +25,23 @@ void SubGraphics::sleep()
 
 void SubGraphics::only_draw()
 {
-	ID3D11DeviceContext* Device_context = get_owner()->this_scene->this_engine->Device_context.Get();
-
-#pragma message (__FILE__ "(" _CRT_STRINGIZE(__LINE__) ")" ": warning: 필요없는 코드")
-	// Graphics에서 설정되는 것들
+	// SubGraphics에서 렌더링 파이프라인 설정 21.11.15
 	Device_context->RSSetViewports(1, &viewport);
-
-	texel_size.data.texel_size.x = 1.f / (float)this->viewport.Width;
-	texel_size.data.texel_size.y = 1.f / (float)this->viewport.Height;
-	texel_size.initialize(this->owner->this_scene->this_engine->Device.Get());
-	texel_size.apply_changes(this->owner->this_scene->this_engine->Device_context.Get());
-	texel_size.set_constant_buffer(Device_context);
 	Camera_ptr->set_camera(this);
 
+	// 드로잉 씬 리스트에 따라 드로잉함. 21.11.15
 	for (auto it = Drawing_scenes.begin(); it != Drawing_scenes.end(); ++it)
 	{
 		if (it->is_vaild())
 		{
 			it->get()->draw_all_renderer(this);
 		}
-		else
-		{
-			// 유효하지 않을때는 어떻게 반응해야 하는가.
-		}
 	}
 }
 
 void SubGraphics::update_viewport_size(FLOAT topLeftX, FLOAT topLeftY, FLOAT width, FLOAT height, FLOAT minDepth, FLOAT maxDepth)
 {
+	// viewport desc 업데이트
 	this->viewport.TopLeftX = topLeftX;
 	this->viewport.TopLeftY = topLeftY;
 	this->viewport.Width = width;
@@ -63,8 +52,10 @@ void SubGraphics::update_viewport_size(FLOAT topLeftX, FLOAT topLeftY, FLOAT wid
 
 void SubGraphics::update_viewport_size(UINT window_width, UINT window_height)
 {
+	// 조건 검사
 	if (window_width < 1 && window_height < 1) return;
 
+	// update_viewport_size 전달
 	update_viewport_size(
 		0.0f,
 		0.0f,
@@ -79,9 +70,11 @@ void SubGraphics::draw_detail_view()
 
 	if (ImGui::CollapsingHeader("Sub graphics"))
 	{
+		// 카메라와 렌더타겟 설정
 		ImGui::base_field(Camera_ptr, "Camera");
 		ImGui::base_field(Render_target, "Render_target");
 
+		// 드로잉 씬 등록
 		static int index;
 		index = 0;
 		ImGui::list_field_default(Drawing_scenes, "Drawing scenes", 
@@ -90,53 +83,5 @@ void SubGraphics::draw_detail_view()
 				ImGui::base_field(scene, std::to_string(index));
 				++index;
 			});
-		//std::string drawing_scenes
-		//	= "drawing scenes##"
-		//	+ StringHelper::ptr_to_string(&Drawing_scenes);
-		//if (ImGui::TreeNode(drawing_scenes.c_str()))
-		//{
-		//	ImGui::NewLine();
-		//	std::string drawing_scenes_add_button
-		//		= "add scene##"
-		//		+ StringHelper::ptr_to_string(&Drawing_scenes);
-		//	if (ImGui::RightAngleButton(drawing_scenes_add_button.c_str()))
-		//	{
-		//		Drawing_scenes.push_back(SafePtr<Scene>());
-		//	}
-		//	auto delete_target = Drawing_scenes.end();
-		//	int index = 0;
-		//	for (auto iter = Drawing_scenes.begin(); iter != Drawing_scenes.end(); ++iter)
-		//	{
-		//		std::string drawing_scenes_add_button
-		//			= "X##"
-		//			+ StringHelper::ptr_to_string(&*iter);
-		//		if (ImGui::Button(drawing_scenes_add_button.c_str()))
-		//		{
-		//			delete_target = iter;
-		//		}
-		//		ImGui::SameLine();
-
-		//		std::string scene_name = std::to_string(index);
-		//		scene_name += "##" + StringHelper::ptr_to_string(&*iter);
-		//		ImGui::base_field(*iter, scene_name.c_str());
-		//		++index;
-		//	}
-		//	if (delete_target != Drawing_scenes.end())
-		//		Drawing_scenes.erase(delete_target);
-		//	ImGui::TreePop();
-		//}
 	}
 }
-
-//
-//void SubGraphics::DrawMeshStruct::draw(SubGraphics* sub_graphics, ConstantBuffer<BTYPE::CB_World>* world_constant_buffer)
-//{
-//	if (this->mesh.is_vaild() == false) return;
-//	if (this->shader.is_vaild() == false) return;
-//
-//	world_constant_buffer->set_constant_buffer(sub_graphics->Device_context);
-//
-//	shader->set_shader(sub_graphics->Device_context);
-//	mesh->set_mesh(sub_graphics->Device_context, Offset);
-//	mesh->draw(sub_graphics->Device_context, 0, 0);
-//}
--- Engine/Component/Graphics/_SubGraphics.h
+++ Engine/Component/Graphics/_SubGraphics.h
@@ -5,30 +5,76 @@
 class Camera;
 class RenderTarget;
 
+/// <summary>
+/// 게임의 렌더를 관리하는 컴포넌트
+/// 21.11.15
+/// </summary>
 class SubGraphics : public Component
 {
+
+#pragma region ConDestructor
+
 public:
-	SafePtr<Camera> Camera_ptr;
-	SafePtr<RenderTarget> Render_target;
-	CD3D11_VIEWPORT viewport;
-
-	std::list<SafePtr<Scene>> Drawing_scenes;
-
-	// 
-	UINT Offset = 0; // 항상 0 이지 않을까 싶음.
-	ID3D11Device* Device = NULL;
-	ID3D11DeviceContext* Device_context = NULL;
-
-
 	SubGraphics() {	}
 	SubGraphics(SafePtr<Camera> Camera_ptr, SafePtr<RenderTarget> Render_target, float resolutionWidth, float resolutionHeight);
 	~SubGraphics();
 
-	//virtual bool awake_condition() override;
-	virtual void awake() override;
-	virtual void sleep() override;
+#pragma endregion
+
+	// 게임 렌더를 위해 필요한 변수
+	// 21.11.15
+#pragma region Val_SubGraphics
+
+public:
+	ID3D11Device* Device = NULL;
+	ID3D11DeviceContext* Device_context = NULL;
+
+#pragma message (__FILE__ "(" _CRT_STRINGIZE(__LINE__) ")" ": warning: viewport를 Camera에서 설정하도록 하면 더 직관적인지에 대해서 고민해봐야함.")
+	// viewport를 렌더타겟에서 설정하도록 하면
+	// update_viewport_size, update_viewport_size의 함수를
+	// 병합하여 보다 단순화 시킬 수 있지 않을까 생각함
+	// 
+	// 실행을 위해서는 검증이 필요함
+	// 
+	// 라이트 지오메트리 버퍼와 칼라 지오메트리 버퍼의 크기를 다르게 하기 위해서
+	// 필요하다고 생각함.
+	// 21.11.15
+	CD3D11_VIEWPORT viewport;
+	SafePtr<Camera> Camera_ptr;
+	SafePtr<RenderTarget> Render_target;
+
+	std::list<SafePtr<Scene>> Drawing_scenes;
+
+	/// <summary>
+	/// draw 필요한 오프셋 변수, 필요할 때 오버라이드 하여 사용함.
+	/// 21.11.15
+	/// </summary>
+	UINT Offset = 0;
+
+#pragma endregion
+
+	// SubGraphics의 메서드
+	// 21.11.15
+#pragma region Func_SubGraphics
+
+public:
+	/// <summary>
+	/// SubGraphics가 가지고 있는 정보를 설정하고
+	/// 드로잉 씬 리스트에 따라 draw_all_renderer를 호출함.
+	/// 21.11.15
+	/// </summary>
 	void only_draw();
 
+	/// <summary>
+	/// viewport desc의 값을 업데이트 하는 메서드
+	/// 21.11.15
+	/// </summary>
+	/// <param name="topLeftX"></param>
+	/// <param name="topLeftY"></param>
+	/// <param name="width"></param>
+	/// <param name="height"></param>
+	/// <param name="minDepth"></param>
+	/// <param name="maxDepth"></param>
 	void update_viewport_size(
 		FLOAT topLeftX,
 		FLOAT topLeftY,
@@ -38,10 +84,26 @@ public:
 		FLOAT maxDepth = D3D11_MAX_DEPTH
 	);
 
+	/// <summary>
+	/// update_viewport_size를 width와 height만으로 업데이트 하는 메서드
+	/// 21.11.15
+	/// </summary>
+	/// <param name="window_width"></param>
+	/// <param name="window_height"></param>
 	void update_viewport_size(
 		UINT window_width,
 		UINT window_height
 	);
 
+#pragma endregion
+
+#pragma region Component
+
+public:
+	virtual void awake() override;
+	virtual void sleep() override;
 	virtual void draw_detail_view() override;
+
+#pragma endregion
+
 };
