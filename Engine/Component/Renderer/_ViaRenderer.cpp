c598792e727030af3644f5ec00d53a8c672eb37b
kbmhansungb <56149613+kbmhansungb@users.noreply.github.com>
2021-11-16 13:09:23 +0900

    ViaRenderer

--- Engine/Component/Renderer/_ViaRenderer.cpp
+++ Engine/Component/Renderer/_ViaRenderer.cpp
@@ -1,1 +1,21 @@
 #include "_ViaRenderer.h"
+
+void ViaRenderer::set_visibility(bool new_visiblity)
+{
+	RenderScope::set_visibility(new_visiblity, this->get_owner()->this_scene.get());
+}
+
+void ViaRenderer::draw_detail_view()
+{
+	Component::draw_detail_view();
+
+	if (ImGui::CollapsingHeader("Via renderer"))
+	{
+		static bool temp_visibility;
+		temp_visibility = this->VIsiblity;
+		if (ImGui::Checkbox("Set visibility", &temp_visibility))
+		{
+			set_visibility(temp_visibility);
+		}
+	}
+}
--- Engine/Component/Renderer/_ViaRenderer.h
+++ Engine/Component/Renderer/_ViaRenderer.h
@@ -2,29 +2,50 @@
 
 #include "../_Include.h"
 
+/// <summary>
+/// 렌더링을 결정하는 컴포넌트.
+/// 씬에 리스트 형식으로 저장되어 렌더링 됨.
+/// 자세한건 기술 문서 참조
+/// 21.11.16
+/// </summary>
 class ViaRenderer : public Component, public RenderScope
 {
-public:
-	// RenderScope을(를) 통해 상속됨
-	virtual void set_visibility(bool new_visiblity) override
-	{
-		RenderScope::set_visibility(new_visiblity, this->get_owner()->this_scene.get());
-	}
-	virtual void draw(SubGraphics* sub_graphics) override = 0;
-	virtual void draw_mesh_only(SubGraphics* sub_graphics) override = 0;
-	virtual void draw_detail_view()
-	{
-		Component::draw_detail_view();
 
-		if (ImGui::CollapsingHeader("Via renderer"))
-		{
-			static bool temp_visibility;
-			temp_visibility = this->VIsiblity;
-			if (ImGui::Checkbox("Set visibility", &temp_visibility))
-			{
-				set_visibility(temp_visibility);
-			}
-		}
-	}
+	// 렌더 하는 함수들 모음
+	// 21.11.16
+#pragma region Func_ViaRenderer
+
+public:
+	/// <summary>
+	/// 비지빌리티를 설정하는 메서드
+	/// 21.11.16
+	/// </summary>
+	/// <param name="new_visiblity"></param>
+	virtual void set_visibility(bool new_visiblity) override;
+
+	/// <summary>
+	/// 파이프라인의 드로우를 호출하는 메서드,
+	/// 머테리얼을 이용함
+	/// 21.11.16
+	/// </summary>
+	/// <param name="sub_graphics"></param>
+	virtual void draw(SubGraphics* sub_graphics) override = 0;
+
+	/// <summary>
+	/// 머테리얼을 외부에서 설정하고 드로우 하는 메서드
+	/// 21.11.16
+	/// </summary>
+	/// <param name="sub_graphics"></param>
+	virtual void draw_mesh_only(SubGraphics* sub_graphics) override = 0;
+
+#pragma endregion
+
+#pragma region Component
+
+public:
+	virtual void draw_detail_view();
+
+#pragma endregion
+
 
 };
